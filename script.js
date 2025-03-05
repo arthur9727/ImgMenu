4122b6a9790e57131476a9fc2d328cde02de16d6
arthur9727 <50811000+arthur9727@users.noreply.github.com>
2025-03-05 14:49:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const imageName = params.get("image");
 
 if (imageName) {
     // Atribui o caminho correto da imagem ao 'src'
-    document.getElementById("displayImage").src = `images/${notify}.png`;  // Ajuste o caminho se necessário
+    document.getElementById("displayImage").src = `images/${imageName}.png`;  // Caminho para a pasta 'images' e imagem .png
 } else {
     console.error("Nenhuma imagem especificada na URL.");
 }
